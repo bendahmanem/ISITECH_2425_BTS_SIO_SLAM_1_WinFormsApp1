575770824973e4e4c9aa54a15980afa53aa3ed77
mouni <mouni@MOON>
2025-02-11 15:12:46 +0100

    TP3

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -34,15 +34,20 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             groupBox1 = new GroupBox();
+            txtErrAge = new Label();
+            txtAge = new TextBox();
+            labelAge = new Label();
             erreurNom = new Label();
             erreurEmail = new Label();
+            dgContacts = new DataGridView();
             groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgContacts).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Enabled = false;
-            button1.Location = new Point(154, 213);
+            button1.Location = new Point(154, 353);
             button1.Margin = new Padding(4);
             button1.Name = "button1";
             button1.Size = new Size(374, 54);
@@ -87,6 +92,9 @@
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(txtErrAge);
+            groupBox1.Controls.Add(txtAge);
+            groupBox1.Controls.Add(labelAge);
             groupBox1.Controls.Add(erreurNom);
             groupBox1.Controls.Add(erreurEmail);
             groupBox1.Controls.Add(textBox2);
@@ -96,10 +104,37 @@
             groupBox1.Controls.Add(label2);
             groupBox1.Location = new Point(187, 100);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(808, 298);
+            groupBox1.Size = new Size(808, 468);
             groupBox1.TabIndex = 5;
             groupBox1.TabStop = false;
             groupBox1.Text = "groupBox1";
+            // 
+            // txtErrAge
+            // 
+            txtErrAge.AutoSize = true;
+            txtErrAge.ForeColor = Color.Red;
+            txtErrAge.Location = new Point(404, 228);
+            txtErrAge.Name = "txtErrAge";
+            txtErrAge.Size = new Size(198, 32);
+            txtErrAge.TabIndex = 9;
+            txtErrAge.Text = "L'age est invalide";
+            txtErrAge.Visible = false;
+            // 
+            // txtAge
+            // 
+            txtAge.Location = new Point(154, 221);
+            txtAge.Name = "txtAge";
+            txtAge.Size = new Size(200, 39);
+            txtAge.TabIndex = 8;
+            // 
+            // labelAge
+            // 
+            labelAge.AutoSize = true;
+            labelAge.Location = new Point(46, 224);
+            labelAge.Name = "labelAge";
+            labelAge.Size = new Size(53, 32);
+            labelAge.TabIndex = 7;
+            labelAge.Text = "age";
             // 
             // erreurNom
             // 
@@ -124,11 +159,25 @@
             erreurEmail.Visible = false;
             erreurEmail.Click += label1_Click;
             // 
+            // dgContacts
+            // 
+            dgContacts.AllowUserToAddRows = false;
+            dgContacts.AllowUserToDeleteRows = false;
+            dgContacts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgContacts.Location = new Point(1117, 100);
+            dgContacts.Name = "dgContacts";
+            dgContacts.ReadOnly = true;
+            dgContacts.RowHeadersWidth = 82;
+            dgContacts.Size = new Size(842, 604);
+            dgContacts.TabIndex = 6;
+            dgContacts.CellContentClick += dgContacts_CellContentClick;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1040, 576);
+            ClientSize = new Size(2010, 992);
+            Controls.Add(dgContacts);
             Controls.Add(groupBox1);
             Margin = new Padding(4);
             Name = "Form1";
@@ -136,6 +185,7 @@
             Load += Form1_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgContacts).EndInit();
             ResumeLayout(false);
         }
 
@@ -149,5 +199,9 @@
         private GroupBox groupBox1;
         private Label erreurEmail;
         private Label erreurNom;
+        private Label txtErrAge;
+        private TextBox txtAge;
+        private Label labelAge;
+        private DataGridView dgContacts;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,12 +1,50 @@
 using System.ComponentModel.Design;
+using System.Data;
+using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        // declaration de la variable personne
+        private List<string[]> personnes;
         public Form1()
         {
             InitializeComponent();
+
+            // Creation du bouton
+            DataGridViewButtonColumn btnSupprimer = new DataGridViewButtonColumn();
+            btnSupprimer.Text = "Supprimer";
+            btnSupprimer.HeaderText = "Action";
+            btnSupprimer.Name = "btnSupprimer";
+            btnSupprimer.UseColumnTextForButtonValue = true; // afficher le texte dans le bouton
+
+
+            dgContacts.AllowUserToAddRows = false;
+
+            personnes = new List<string[]>
+            {
+                new string[] { "Jean", "25" },
+                new string[] { "Paul", "30" },
+                new string[] { "Marie", "28" }
+            };
+
+            DataTable dataTable = new DataTable();
+
+
+            dataTable.Columns.Add("Nom", typeof(string));
+            dataTable.Columns.Add("Age", typeof(string));
+
+            foreach (string[] personne in personnes)
+            {
+                dataTable.Rows.Add(personne[0], personne[1]);
+            }
+
+            this.dgContacts.DataSource = dataTable;
+            this.dgContacts.Columns.Add(btnSupprimer);
+
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -16,14 +54,29 @@ namespace WinFormsApp1
             // type nom = <valeur>;
             string nom = textBox1.Text; // txtNom.Text;
             string email = textBox2.Text; // txtEmail.Text;
+            string age = this.txtAge.Text;
 
-            if (nom == "" || email == "")
+            if (nom == "" || email == "" || age == "")
             {
                 MessageBox.Show("Merci de remplir  tous les champs!");
             }
             else
             {
-                MessageBox.Show(" Le contact " + nom + ", " + email + " a bien ete ajoute!");
+                // Ajout d'une nouvelle personne
+                personnes.Add(new string[] { nom, age });
+
+                DataTable dataTable = new DataTable();
+
+
+                dataTable.Columns.Add("Nom", typeof(string));
+                dataTable.Columns.Add("Age", typeof(string));
+
+                foreach (string[] personne in personnes)
+                {
+                    dataTable.Rows.Add(personne[0], personne[1]);
+                }
+
+                this.dgContacts.DataSource = dataTable;
             }
             // Afficher ces valeurs dans une messageBox
         }
@@ -71,10 +124,38 @@ namespace WinFormsApp1
             if (textBox2.Text != "" && textBox1.Text.Contains("@"))
             {
                 this.button1.Enabled = true;
-            } else
+            }
+            else
             {
                 this.button1.Enabled = false;
             }
         }
+
+        private void dgContacts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Console.WriteLine("Cell clicked");
+
+            if (e.ColumnIndex == dgContacts.Columns["btnSupprimer"].Index)
+            {
+                personnes.RemoveAt(e.RowIndex);
+
+
+                DataTable dataTable = new DataTable();
+
+
+                dataTable.Columns.Add("Nom", typeof(string));
+                dataTable.Columns.Add("Age", typeof(string));
+
+                foreach (string[] personne in personnes)
+                {
+                    dataTable.Rows.Add(personne[0], personne[1]);
+                }
+
+                this.dgContacts.DataSource = dataTable;
+
+            }
+        }
+
+
     }
 }
